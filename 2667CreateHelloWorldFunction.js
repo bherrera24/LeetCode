6dac3091ae1a31f3cc5ccdac5d1ebae31764269a
bherrera24 <bherreran24@gmail.com>
2024-02-22 00:00:15 -0300

    Create 2667CreateHelloWorldFunction.js

---
+++ 2667CreateHelloWorldFunction.js
@@ -0,0 +1,5 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+/**
+ * @return {Function}
+ */
+const createHelloWorld = () => (...args)=> "Hello World"
