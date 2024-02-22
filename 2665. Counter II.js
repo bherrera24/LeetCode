957af19924d6a63b8aa91b4acc8216c6d60d2a93
bherrera24 <bherreran24@gmail.com>
2024-02-21 23:46:48 -0300

    Create 2665. Counter II.js

---
+++ 2665. Counter II.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+ let i=-1
+const createCounter = function(init) {
+    let count = init
+
+    const increment= ()=> ++count
+    const decrement= ()=> --count
+    const reset= ()=> {
+        count= init
+        return count
+    }
+    return {
+        increment,
+        decrement,
+        reset
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
